3bb9f40da188b4d5d15d5f1ba4c88b0655521f5f
Roderic Linguri <rod@linguri.com>
2015-07-11 14:51:14 -0600

    Add abstract placeholders

---
+++ lib.php
@@ -0,0 +1,26 @@
+<?php
+
+abstract class AMVObject
+{
+
+}
+
+abstract class AMVDatabase
+{
+
+}
+
+abstract class AMVModel
+{
+
+}
+
+abstract class AMVView
+{
+
+}
+
+abstract class AMVController
+{
+
+}
