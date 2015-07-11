bbdd454a727dc75568f95a3af269294833a755ee
Roderic Linguri <rod@linguri.com>
2015-07-11 16:00:19 -0600

    Add initial database functionality

--- lib.php
+++ lib.php
@@ -1,5 +1,8 @@
 <?php
 
+// define('AMV_MODE', 'DEVELOPMENT');
+define('AMV_MODE', 'PRODUCTION');
+
 abstract class AMVObject
 {
 
@@ -28,6 +31,171 @@ abstract class AMVObject
 
 abstract class AMVDatabase
 {
+
+    /* @property PDO connection */
+    protected $pdo;
+    
+    /* @property the PDO statement handle */
+    protected $stmt;
+    
+    /* @property array of arguments to pass to statement */
+    protected $args;
+
+/**** ADD THE FOLLOWING TO EXTENDED CLASSES
+
+    // database singleton
+    protected static $db;
+
+    // singleton getter
+    public static function db()
+    {
+        if (!isset(self::$db)) {
+        
+            self::$db = new self();
+            
+        }
+        
+        return self::$db;
+        
+    }
+
+    // constructor
+    public function __construct()
+    {
+
+        $this->pdo = new PDO('mysql:host=localhost;dbname=testdb','root','password');
+        
+        // production 
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+        // production 
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+    }
+
+****/
+
+    /***
+     * set the statement
+     * @param:  string (prepared statement)
+     * @return: bool
+     */
+    public function setStmt()
+    {
+        $args = func_get_args();
+        
+        if (count($args) > 0) {
+        
+            $this->stmt = array_shift($args);
+            
+        }
+        
+        // any extraneous parameters are assumed to be statement parameters
+        if (count($args) > 0) {
+        
+            $this->setArgs($args);
+        
+        }
+
+        if (isset($this->stmt)) {
+        
+            return true;
+            
+        } else {
+        
+            return false;
+            
+        }
+        
+    }
+
+    /***
+     * set the statement arguments
+     * @param:  string, [string]
+     * @return: bool
+     */
+    public function setArgs()
+    {
+        $args = func_get_args();
+        
+        if (count($args) > 0) {
+        
+            $this->args = $args;
+            
+        } else {
+        
+            // NOTE: if no args are passed, any existing args are erased
+            $this->args = null;
+            
+        }
+
+    }
+
+    /***
+     * execute statement with passed in arguments
+     * @param:  string, [string]
+     * @return: bool
+     */
+    public function executeStmt()
+    {
+    
+        $args = func_get_args();
+        
+        if (isset($this->stmt)) {
+
+            if (count($args) > 0)) {
+            
+                if ($this->stmt->execute($args) == true) {
+                
+                    return true;
+                    
+                } else {
+                
+                    return false;
+                    
+                }
+                
+            } else {
+            
+                if ($this->stmt->execute() == true) {
+                
+                    return true;
+                    
+                } else {
+                
+                    return false;
+                    
+                }
+            
+            }
+
+        } else {
+        
+            return false;
+        
+        }
+
+    }
+
+    public function fetchRecord()
+    {
+    
+    }
+
+    public function fetchRecords()
+    {
+    
+    }
+
+    public function insertRecord()
+    {
+    
+    }
+
+    public function insertRecords()
+    {
+    
+    }
 
 }
 
